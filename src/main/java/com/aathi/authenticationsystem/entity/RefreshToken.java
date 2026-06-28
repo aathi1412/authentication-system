c9ad87f59d246412f71661aedc56330715ceeb2f
Aathi <aathithirunav@gmail.com>
2026-06-28 14:40:45 +0530

    add and implement a Refresh token Entity

---
+++ src/main/java/com/aathi/authenticationsystem/entity/RefreshToken.java
@@ -0,0 +1,27 @@
+package com.aathi.authenticationsystem.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String token;
+
+    private LocalDateTime expiry;
+
+    private User user;
+}
