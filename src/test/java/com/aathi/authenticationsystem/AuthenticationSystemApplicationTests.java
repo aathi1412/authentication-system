6d8328d5aa230ac4b4f5e378fa9f99298c8a546f
Aathi <aathithirunav@gmail.com>
2026-06-28 14:38:51 +0530

    setup maven project

---
+++ src/test/java/com/aathi/authenticationsystem/AuthenticationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aathi.authenticationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
