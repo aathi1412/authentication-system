b72dddf735c212701a40f2807d30dfd55ac8deac
Aathi <aathithirunav@gmail.com>
2026-06-28 15:25:25 +0530

    add and implement a verification token Entity and add relationship mapping to user table

---
+++ src/main/java/com/aathi/authenticationsystem/entity/VerificationToken.java
@@ -0,0 +1,28 @@
+package com.aathi.authenticationsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class VerificationToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(nullable = false)
+    private LocalDateTime expiryDate;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
