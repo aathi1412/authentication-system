62a03a3c475c4e98156f912f0fe57204e10bdebd
Aathi <aathithirunav@gmail.com>
2026-06-28 15:24:58 +0530

    add and implement a password token Entity and add relationship mapping to user table

---
+++ src/main/java/com/aathi/authenticationsystem/entity/PasswordResetToken.java
@@ -0,0 +1,28 @@
+package com.aathi.authenticationsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class PasswordResetToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(nullable = false)
+    private LocalDateTime expiryDate;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
