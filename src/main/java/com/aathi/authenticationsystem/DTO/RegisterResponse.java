4ab8a343f059c4eceaa95c852114eefa4e9f382d
Aathi <aathithirunav@gmail.com>
2026-06-29 19:58:53 +0530

    feat(auth): Add RegisterResponse DTO
    - Introduce DTO to encapsulate registration success message and user details

---
+++ src/main/java/com/aathi/authenticationsystem/DTO/RegisterResponse.java
@@ -0,0 +1,16 @@
+package com.aathi.authenticationsystem.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterResponse {
+
+    private String message;
+    private UserResponse userResponse;
+}
