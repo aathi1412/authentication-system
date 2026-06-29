6814efa99e10b9f5895e84796d03df17858ff767
Aathi <aathithirunav@gmail.com>
2026-06-29 20:00:10 +0530

    feat(auth): Add LoginRequest DTO
    - Defines fields for user email and password.
    - Incorporates `jakarta.validation` annotations for input validation.

---
+++ src/main/java/com/aathi/authenticationsystem/DTO/LoginRequest.java
@@ -0,0 +1,18 @@
+package com.aathi.authenticationsystem.DTO;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+
+    @NotBlank(message = "Email is Required")
+    @Email(message = "Invalid Email")
+    private String email;
+
+    @Size(min = 8, message = "password must be at least 8 characters")
+    private String password;
+
+}
