87b11f0d40dbd6ccd67e54680a93b15a613ea29b
Aathi <aathithirunav@gmail.com>
2026-06-29 19:54:25 +0530

    feat(auth): Implement global exception handler for email conflicts
    - Centralize exception handling using `@RestControllerAdvice`.
    - Catch `EmailAlreadyExistsException` to provide specific error responses.
    - Return a standardized `ErrorResponse` with `HttpStatus.CONFLICT`.

---
+++ src/main/java/com/aathi/authenticationsystem/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.aathi.authenticationsystem.exception;
+
+import com.aathi.authenticationsystem.DTO.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends RuntimeException{
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> handleEmailAlreadyExistsException(EmailAlreadyExistsException ex){
+        ErrorResponse error = ErrorResponse.builder()
+                .timeStamp(LocalDateTime.now())
+                .status(HttpStatus.CONFLICT.value())
+                .error(HttpStatus.CONFLICT.getReasonPhrase())
+                .message(ex.getMessage())
+                .build();
+
+         return new ResponseEntity<>(error, HttpStatus.CONFLICT);
+    }
+}
