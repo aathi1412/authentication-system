061e91f1b0710becfe8a04fd4e6daaa410b870c3
Aathi <aathithirunav@gmail.com>
2026-06-29 19:59:06 +0530

    feat(auth): Add RegisterRequest DTO
    - Defines the data structure for user registration requests.
    - Includes validation constraints for name, email, and password fields.

---
+++ src/main/java/com/aathi/authenticationsystem/DTO/RegisterRequest.java
@@ -0,0 +1,21 @@
+package com.aathi.authenticationsystem.DTO;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotBlank(message = "Email is Required")
+    @Email(message = "Invalid Email")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+}
