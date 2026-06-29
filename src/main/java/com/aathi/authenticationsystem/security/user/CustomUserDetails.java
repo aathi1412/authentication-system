74171539a2a74faa86fc7fb837416f4d7abc2e24
Aathi <aathithirunav@gmail.com>
2026-06-29 19:51:39 +0530

    feat(auth): Add CustomUserDetails for Spring Security integration
    - Implement Spring Security's UserDetails interface
    - Wrap the custom User entity to provide user details
    - Enable Spring Security to authenticate and authorize users based on application's User model

---
+++ src/main/java/com/aathi/authenticationsystem/security/user/CustomUserDetails.java
@@ -0,0 +1,45 @@
+package com.aathi.authenticationsystem.security.user;
+
+import com.aathi.authenticationsystem.entity.User;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Builder
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(user.getRole().name()));
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getName();
+    }
+
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return user.isAccountLocked();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isEnabled();
+    }
+}
