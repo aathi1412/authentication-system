f448b7582ccfe4129a131f2415efd426d072623e
Aathi <aathithirunav@gmail.com>
2026-06-29 19:50:11 +0530

    feat(auth): Add AuthController with register and login endpoints
    - Introduce `AuthController` to handle user authentication requests.
    - Implement `/api/auth/register` endpoint for user registration.
    - Ensure registration returns `201 Created` status upon successful user creation.
    - Add placeholder `/api/auth/login` endpoint for future login functionality.
    - Utilize `@Valid` for request body validation on the registration endpoint.

---
+++ src/main/java/com/aathi/authenticationsystem/controller/AuthController.java
@@ -0,0 +1,36 @@
+package com.aathi.authenticationsystem.controller;
+
+import com.aathi.authenticationsystem.DTO.LoginRequest;
+import com.aathi.authenticationsystem.DTO.RegisterRequest;
+import com.aathi.authenticationsystem.DTO.RegisterResponse;
+import com.aathi.authenticationsystem.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> registerRequest(@Valid @RequestBody RegisterRequest request){
+        RegisterResponse response = authService.register(request);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+    }
+
+    @PostMapping("/login")
+    public String loginRequest(@RequestBody LoginRequest request){
+        return "";
+    }
+}
