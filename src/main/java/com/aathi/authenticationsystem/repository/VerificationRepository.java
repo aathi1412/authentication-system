dac14a4ae8a79b3fe862d72548abb9892aaacd2b
Aathi <aathithirunav@gmail.com>
2026-06-29 19:55:48 +0530

    feat(auth): Add token lookup to verification repository

--- src/main/java/com/aathi/authenticationsystem/repository/VerificationRepository.java
+++ src/main/java/com/aathi/authenticationsystem/repository/VerificationRepository.java
@@ -4,6 +4,9 @@ import com.aathi.authenticationsystem.entity.VerificationToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface VerificationRepository extends JpaRepository<VerificationToken, Long> {
+    Optional< VerificationToken> findByToken(String token);
 }
