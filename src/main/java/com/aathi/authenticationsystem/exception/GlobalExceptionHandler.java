9948287161ba5554fbaaf8f77b336f1c1c99f673
Aathi <aathithirunav@gmail.com>
2026-06-30 23:29:27 +0530

    feat(exception): Add exception handlers
    - Handle AccountNotVerifiedException
    - Handle InvalidRefreshTokenException

--- src/main/java/com/aathi/authenticationsystem/exception/GlobalExceptionHandler.java
+++ src/main/java/com/aathi/authenticationsystem/exception/GlobalExceptionHandler.java
@@ -1,13 +1,12 @@
 package com.aathi.authenticationsystem.exception;
 
-import com.aathi.authenticationsystem.DTO.ErrorResponse;
+import com.aathi.authenticationsystem.dto.response.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends RuntimeException{
@@ -35,4 +34,29 @@ public class GlobalExceptionHandler extends RuntimeException{
 
         return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
     }
+
+    @ExceptionHandler(AccountNotVerifiedException.class)
+    public ResponseEntity<ErrorResponse> handleAccountNotVerifiedException(AccountNotVerifiedException ex){
+        ErrorResponse error = ErrorResponse.builder()
+                .timeStamp(Instant.now())
+                .status(HttpStatus.FORBIDDEN.value())
+                .error(HttpStatus.FORBIDDEN.getReasonPhrase())
+                .message(ex.getMessage())
+                .build();
+
+        return new ResponseEntity<>(error, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(InvalidRefreshTokenException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidRefreshTokenException(InvalidRefreshTokenException ex){
+        ErrorResponse error = ErrorResponse.builder()
+                .timeStamp(Instant.now())
+                .status(HttpStatus.UNAUTHORIZED.value())
+                .error(HttpStatus.UNAUTHORIZED.getReasonPhrase())
+                .message(ex.getMessage())
+                .build();
+
+        return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
+    }
+
 }
