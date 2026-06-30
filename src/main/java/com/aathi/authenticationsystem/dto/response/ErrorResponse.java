3da0c0ad64597dc3464d275038b554711bf2871a
Aathi <aathithirunav@gmail.com>
2026-06-30 23:29:21 +0530

    refactor(dto): Rename ErrorResponse package
    - Renamed ErrorResponse package from com.aathi.authenticationsystem.DTO to com.aathi.authenticationsystem.dto.response

--- src/main/java/com/aathi/authenticationsystem/dto/response/ErrorResponse.java
+++ src/main/java/com/aathi/authenticationsystem/dto/response/ErrorResponse.java
@@ -1,4 +1,4 @@
-package com.aathi.authenticationsystem.DTO;
+package com.aathi.authenticationsystem.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -6,7 +6,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
 
 @Data
 @Builder
