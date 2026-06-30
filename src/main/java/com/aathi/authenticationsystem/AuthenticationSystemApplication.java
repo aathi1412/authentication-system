9038b52410f262e29692b3750a147c429fd096ce
Aathi <aathithirunav@gmail.com>
2026-06-30 14:36:20 +0530

    refactor(config): Enable configuration properties scan
    - Add @ConfigurationPropertiesScan annotation to AuthenticationSystemApplication

--- src/main/java/com/aathi/authenticationsystem/AuthenticationSystemApplication.java
+++ src/main/java/com/aathi/authenticationsystem/AuthenticationSystemApplication.java
@@ -2,8 +2,10 @@ package com.aathi.authenticationsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class AuthenticationSystemApplication {
 
     public static void main(String[] args) {
