972c1a7f85b0cd01eca07c5f6c63dbfb79f7893a
Aathi <aathithirunav@gmail.com>
2026-06-30 14:36:44 +0530

    feat(security): Add JwtFilter
    - Implement JWT token validation
    - Integrate with CustomUserDetailsService
    - Handle unauthorized requests

---
+++ src/main/java/com/aathi/authenticationsystem/security/JwtFilter.java
@@ -0,0 +1,62 @@
+package com.aathi.authenticationsystem.security;
+
+import com.aathi.authenticationsystem.service.CustomUserDetailsService;
+import com.aathi.authenticationsystem.service.JwtService;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final CustomUserDetailsService customUserDetailsService;
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String header = request.getHeader("Authorization");
+        if(header == null || !header.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = header.substring(7);
+
+        try {
+            String email = jwtService.extractEmail(token);
+
+            if(email != null && SecurityContextHolder.getContext().getAuthentication() == null){
+
+                CustomUserDetails customUserDetails = customUserDetailsService.loadUserByUsername(email);
+
+                if(jwtService.validateToken(token, customUserDetails)){
+                    UsernamePasswordAuthenticationToken authenticationToken =
+                            new UsernamePasswordAuthenticationToken(
+                                    customUserDetails,
+                                    null,
+                                    customUserDetails.getAuthorities());
+                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }
+            }
+        } catch (JwtException ex){
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
