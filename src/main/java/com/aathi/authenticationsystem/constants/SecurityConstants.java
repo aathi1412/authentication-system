16698879a9ce988901147ad0072155b4b0f53d8a
Aathi <aathithirunav@gmail.com>
2026-07-01 21:21:16 +0530

    feat(security): Add SecurityConstants
    - Introduced SecurityConstants class to hold security-related constants

---
+++ src/main/java/com/aathi/authenticationsystem/constants/SecurityConstants.java
@@ -0,0 +1,9 @@
+package com.aathi.authenticationsystem.constants;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityConstants {
+    public static final String REFRESH_TOKEN_COOKIE = "refreshToken";
+
+}
