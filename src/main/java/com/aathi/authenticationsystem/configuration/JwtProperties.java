d0979bd003e889084e6c6b6827622027e8963d6d
Aathi <aathithirunav@gmail.com>
2026-07-01 21:20:16 +0530

    feat(security): Add Jwt Expiration Properties
    - Added accessTokenExpiration property to JwtProperties
    - Added refreshTokenExpiration property to JwtProperties

--- src/main/java/com/aathi/authenticationsystem/configuration/JwtProperties.java
+++ src/main/java/com/aathi/authenticationsystem/configuration/JwtProperties.java
@@ -2,8 +2,12 @@ package com.aathi.authenticationsystem.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.time.Duration;
+
 @ConfigurationProperties(prefix = "jwt")
 public record JwtProperties(
 
-        String secretKey
+        String secretKey,
+        Duration accessTokenExpiration,
+        Duration refreshTokenExpiration
 ) {}
