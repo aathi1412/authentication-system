705dd05707f52f0573ea5dee8270dcf9bf249ca9
Aathi <aathithirunav@gmail.com>
2026-07-02 11:32:21 +0530

    feat(security): Add cookie constants
    - Introduced CookieConstants class to store cookie-related constants

---
+++ src/main/java/com/aathi/authenticationsystem/constants/CookieConstants.java
@@ -0,0 +1,11 @@
+package com.aathi.authenticationsystem.constants;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public final class CookieConstants {
+    public static final String REFRESH_TOKEN_COOKIE = "refreshToken";
+
+    private CookieConstants(){}
+
+}
