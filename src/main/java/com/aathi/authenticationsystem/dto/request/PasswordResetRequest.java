a9f718a7666ef4daa63706991cb18e3cd8ac81c1
Aathi <aathithirunav@gmail.com>
2026-07-03 20:19:48 +0530

    feat(security): Add PasswordResetRequest
    - Introduced a new request DTO for password reset functionality
    - Included validation for the new password field

---
+++ src/main/java/com/aathi/authenticationsystem/dto/request/PasswordResetRequest.java
@@ -0,0 +1,13 @@
+package com.aathi.authenticationsystem.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record PasswordResetRequest(
+
+        String token,
+
+        @NotBlank
+        @Size(min = 8)
+        String newPassword
+) {}
