830119ab184e082f33cf1c1fecb48a9317841e14
Aathi <aathithirunav@gmail.com>
2026-07-03 20:18:42 +0530

    fix(security): Correct isEnabled Logic
    - Fixed the isEnabled method to return the correct user status

--- src/main/java/com/aathi/authenticationsystem/security/userdetails/CustomUserDetails.java
+++ src/main/java/com/aathi/authenticationsystem/security/userdetails/CustomUserDetails.java
@@ -43,7 +43,7 @@ public class CustomUserDetails implements UserDetails {
 
     @Override
     public boolean isEnabled() {
-        return !user.isEnabled();
+        return user.isEnabled();
     }
 
     public long getId(){
