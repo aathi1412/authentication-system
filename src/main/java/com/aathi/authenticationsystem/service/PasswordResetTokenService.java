dfb146ae6ed54a42e334752229141de73afd4246
Aathi <aathithirunav@gmail.com>
2026-07-03 20:19:59 +0530

    feat(security): Add PasswordResetTokenService
    - Create service to manage password reset tokens
    - Generate and verify password reset tokens
    - Handle token expiration

---
+++ src/main/java/com/aathi/authenticationsystem/service/PasswordResetTokenService.java
@@ -0,0 +1,53 @@
+package com.aathi.authenticationsystem.service;
+
+import com.aathi.authenticationsystem.entity.PasswordResetToken;
+import com.aathi.authenticationsystem.entity.User;
+import com.aathi.authenticationsystem.exception.InvalidPasswordResetTokenException;
+import com.aathi.authenticationsystem.repository.PasswordResetTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class PasswordResetTokenService {
+
+    private final PasswordResetTokenRepository passwordResetTokenRepository;
+
+    @Transactional
+    public PasswordResetToken createOrReplacePasswordResetToken(User user){
+
+        PasswordResetToken passwordResetToken = passwordResetTokenRepository.findByUser(user)
+                .orElseGet(() -> PasswordResetToken.builder()
+                        .createdAt(Instant.now())
+                        .user(user)
+                        .build());
+
+        passwordResetToken.setToken(UUID.randomUUID().toString());
+        passwordResetToken.setExpiryDate(Instant.now().plus(15, ChronoUnit.MINUTES));
+
+        passwordResetTokenRepository.save(passwordResetToken);
+
+         return passwordResetToken;
+    }
+
+    public PasswordResetToken verifyPasswordResetToken(String token){
+        PasswordResetToken resetToken = passwordResetTokenRepository.findByToken(token)
+                .orElseThrow(() -> new InvalidPasswordResetTokenException("Invalid or expired reset token."));
+
+        if(resetToken.isExpired()){
+            throw new InvalidPasswordResetTokenException("Invalid or expired reset token.");
+        }
+
+        return resetToken;
+    }
+
+    @Transactional
+    public void deletePasswordResetToken(PasswordResetToken token){
+        passwordResetTokenRepository.delete(token);
+    }
+}
