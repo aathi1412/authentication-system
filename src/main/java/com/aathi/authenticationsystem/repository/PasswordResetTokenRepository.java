85ae480a347a53828461ce461371c74f3570c2b4
Aathi <aathithirunav@gmail.com>
2026-07-03 20:20:06 +0530

    feat(security): Add findByUser method
    - Added a new method to find password reset tokens by user

--- src/main/java/com/aathi/authenticationsystem/repository/PasswordResetTokenRepository.java
+++ src/main/java/com/aathi/authenticationsystem/repository/PasswordResetTokenRepository.java
@@ -1,6 +1,7 @@
 package com.aathi.authenticationsystem.repository;
 
 import com.aathi.authenticationsystem.entity.PasswordResetToken;
+import com.aathi.authenticationsystem.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,6 @@ import java.util.Optional;
 @Repository
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
     Optional<PasswordResetToken> findByToken(String token);
+
+    Optional<PasswordResetToken> findByUser(User user);
 }
