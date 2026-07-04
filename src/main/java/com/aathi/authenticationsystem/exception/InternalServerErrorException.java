3a35ffe972ced95d83055128163e2b247e8dd990
Aathi <aathithirunav@gmail.com>
2026-07-04 22:34:32 +0530

    feat(exception): Add InternalServerErrorException
    - Created a new custom exception for internal server errors

---
+++ src/main/java/com/aathi/authenticationsystem/exception/InternalServerErrorException.java
@@ -0,0 +1,7 @@
+package com.aathi.authenticationsystem.exception;
+
+public class InternalServerErrorException extends RuntimeException {
+    public InternalServerErrorException(String message) {
+        super(message);
+    }
+}
