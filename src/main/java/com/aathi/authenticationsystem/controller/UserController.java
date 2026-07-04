508a729b87b5b4e05ccdd6a06a3ed28647cf33ff
Aathi <aathithirunav@gmail.com>
2026-07-04 22:34:56 +0530

    refactor(controller): Rename HelloController
    - Renamed HelloController to UserController
    - Updated RequestMapping from /api to /api/user

--- src/main/java/com/aathi/authenticationsystem/controller/UserController.java
+++ src/main/java/com/aathi/authenticationsystem/controller/UserController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
-public class HelloController {
+@RequestMapping("/api/user")
+public class UserController {
 
     @GetMapping("/home")
     public String getHomePage(){
