86c21e56f8d477e3c686ebfd8c25d5ce6f3d3c98
Aathi <aathithirunav@gmail.com>
2026-07-04 21:19:55 +0530

    fix(security): Remove trailing space from token type
    - Removed unnecessary space from the Bearer token type

--- src/main/java/com/aathi/authenticationsystem/constants/SecurityConstants.java
+++ src/main/java/com/aathi/authenticationsystem/constants/SecurityConstants.java
@@ -3,7 +3,7 @@ package com.aathi.authenticationsystem.constants;
 public final class SecurityConstants {
 
     public static final String AUTHORIZATION = "Authorization";
-    public static final String TOKEN_TYPE = "Bearer ";
+    public static final String TOKEN_TYPE = "Bearer";
 
     private SecurityConstants(){}
 }
