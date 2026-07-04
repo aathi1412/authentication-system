6db54b0739e78abba2c575d12c0290850fb6e091
Aathi <aathithirunav@gmail.com>
2026-07-04 21:19:37 +0530

    feat(controller): Add new routes
    - Added /home route
    - Added /dashboard route
    - Added /about route
    - Added /contact route

--- src/main/java/com/aathi/authenticationsystem/controller/HelloController.java
+++ src/main/java/com/aathi/authenticationsystem/controller/HelloController.java
@@ -6,11 +6,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/success")
+@RequestMapping("/api")
 public class HelloController {
 
-    @GetMapping("")
+    @GetMapping("/home")
     public String getHomePage(){
         return "Welcome Login Successfull!";
+    }
+
+    @GetMapping("/dashboard")
+    public String getDashboard(){
+        return "Dashboard!";
+    }
+
+    @GetMapping("/about")
+    public String getAbout(){
+        return "About!";
+    }
+
+    @GetMapping("/contact")
+    public String getContact(){
+        return "Contact!";
     }
 }
