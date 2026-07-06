02a6a11d9faf05f9904115ede318ef18ff50923d
Aathi <aathithirunav@gmail.com>
2026-07-06 23:34:59 +0530

    refactor(entity): Improve PasswordResetToken
    - Add unique constraints for token and user_id
    - Update foreign key for user_id

--- src/main/java/com/aathi/authenticationsystem/entity/PasswordResetToken.java
+++ src/main/java/com/aathi/authenticationsystem/entity/PasswordResetToken.java
@@ -11,6 +11,13 @@ import java.time.Instant;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(
+        name = "password_reset_token",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "UK_password_reset_token", columnNames = "token"),
+                @UniqueConstraint(name = "UK_password_reset_token_user", columnNames = "user_id")
+        }
+)
 public class PasswordResetToken {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,8 +36,10 @@ public class PasswordResetToken {
     @Column(nullable = false)
     private Instant createdAt;
 
-    @SuppressWarnings("JpaDataSourceORMInspection")
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(
+            name = "user_id",
+            foreignKey = @ForeignKey(name = "FK_password_reset_token_user")
+    )
     private User user;
 }
