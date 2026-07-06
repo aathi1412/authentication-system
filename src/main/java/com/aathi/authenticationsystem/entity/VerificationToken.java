25c72ec53e7179e99d1f8d9da2bded544bb29497
Aathi <aathithirunav@gmail.com>
2026-07-06 23:35:51 +0530

    refactor(entity): Add unique constraints to VerificationToken
    - Added unique constraint on token column
    - Added unique constraint on user_id column
    - Updated foreign key constraint on user_id column

--- src/main/java/com/aathi/authenticationsystem/entity/VerificationToken.java
+++ src/main/java/com/aathi/authenticationsystem/entity/VerificationToken.java
@@ -13,6 +13,13 @@ import java.time.Instant;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(
+        name = "verification_token",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "UK_verification_token", columnNames = "token"),
+                @UniqueConstraint(name = "UK_verification_token_user", columnNames = "user_id")
+        }
+)
 public class VerificationToken {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,8 +38,11 @@ public class VerificationToken {
     @Column(nullable = false)
     private Instant createdAt;
 
-    @SuppressWarnings("JpaDataSourceORMInspection")
+
     @OneToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(
+            name = "user_id",
+            foreignKey = @ForeignKey(name = "FK_verification_token_user")
+    )
     private User user;
 }
