ff85a20c6a9f1603456e296a13c3f5054817b757
Aathi <aathithirunav@gmail.com>
2026-07-07 21:13:30 +0530

    refactor(entity-models): Replace entity with models
    - Updated UserRepository to import User from models package instead of entity

--- src/main/java/com/aathi/authenticationsystem/repository/UserRepository.java
+++ src/main/java/com/aathi/authenticationsystem/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.aathi.authenticationsystem.repository;
 
-import com.aathi.authenticationsystem.entity.User;
+import com.aathi.authenticationsystem.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
