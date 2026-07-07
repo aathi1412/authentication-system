eb7b119487c1a7c1662a69128bc64d7527e76c11
Aathi <aathithirunav@gmail.com>
2026-07-07 23:06:14 +0530

    test(authentication-system): Add Calculator Test
    - Added a test case for the Calculator class
    - Verified the add method of the Calculator class

--- src/test/java/com/aathi/authenticationsystem/AuthenticationSystemApplicationTests.java
+++ src/test/java/com/aathi/authenticationsystem/AuthenticationSystemApplicationTests.java
@@ -3,11 +3,24 @@ package com.aathi.authenticationsystem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 @SpringBootTest
 class AuthenticationSystemApplicationTests {
 
     @Test
-    void contextLoads() {
+    void addTwoNumbers() {
+
+        int actual = Calculator.add(10, 20);
+        int expected = 30;
+
+        assertThat(expected).isEqualTo(actual);
+    }
+
+    static class Calculator{
+        static int add (int a, int b){
+            return a + b;
+        }
     }
 
 }
