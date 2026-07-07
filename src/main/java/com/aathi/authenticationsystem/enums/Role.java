b000b1733b53b66055dc450a15c8e3bd46e77557
Aathi <aathithirunav@gmail.com>
2026-07-07 21:04:27 +0530

    feat(security-config): Add Super Admin Role
    - Added SUPER_ADMIN role with permissions to add admin, read, write, and delete todo items

--- src/main/java/com/aathi/authenticationsystem/enums/Role.java
+++ src/main/java/com/aathi/authenticationsystem/enums/Role.java
@@ -7,6 +7,8 @@ import java.util.Set;
 
 @Getter
 public enum Role {
+
+    SUPER_ADMIN(Set.of(Permissions.ADD_ADMIN, Permissions.READ_TODO, Permissions.WRITE_TODO, Permissions.DELETE_TODO)),
     ADMIN(Set.of(Permissions.READ_TODO, Permissions.WRITE_TODO, Permissions.DELETE_TODO)),
     USER(Set.of(Permissions.READ_TODO));
 
