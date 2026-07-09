6eb1dbd03cafba9facdf5ff2d49f990358ba33bf
Aathi <aathithirunav@gmail.com>
2026-07-09 14:13:13 +0530

    feat(src): Improve EmailPasswordButton
    - Add input labels for email and password fields
    - Update input fields with placeholder text and styling

--- src/components/EmailPasswordButton.jsx
+++ src/components/EmailPasswordButton.jsx
@@ -1,8 +1,22 @@
 export function EmailPasswordButton(){
     return (
         <>
-            <input type="text" placeholder="Enter your Email"/>
-            <input type="password" placeholder="Enter your Password"/>
+            <div>
+                <label className="block mb-1">Email</label>
+                <input
+                    type="text"
+                    placeholder="ex: name@gmail.com"
+                    className="placeholder:text-sm border rounded-md py-2 px-2 mb-4 w-70"
+                />
+            </div>
+            <div>
+                <label className="block mb-1">Password</label>
+                <input
+                    type="password"
+                    placeholder="create a Password"
+                    className="placeholder:text-sm border rounded-md py-2 px-2 mb-4 w-70"
+                />
+            </div>
 
             <button>Sign Up</button>
         </>
