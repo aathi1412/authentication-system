0316e4ec85204344462f3912ba1307ad3a3f1251
Aathi <aathithirunav@gmail.com>
2026-07-09 16:29:35 +0530

    feat(src): Add password visibility toggle
    - Added a password visibility toggle button to the password input field
    - Updated input field styles for better consistency and design

--- src/components/EmailPasswordButton.jsx
+++ src/components/EmailPasswordButton.jsx
@@ -1,4 +1,9 @@
+import {useState} from "react";
+import {FiEye, FiEyeOff} from "react-icons/fi";
+
 export function EmailPasswordButton(){
+    const [showPassword, setShowPassword] = useState(false)
+
     return (
         <>
             <div>
@@ -6,19 +11,27 @@ export function EmailPasswordButton(){
                 <input
                     type="text"
                     placeholder="ex: name@gmail.com"
-                    className="placeholder:text-sm border rounded-md py-2 px-2 mb-4 w-70"
+                    className="bg-white placeholder:text-sm w-full
+                            border border-gray-300 outline-none rounded-md py-2 px-2 mb-4
+                            "
                 />
             </div>
             <div>
                 <label className="block mb-1">Password</label>
-                <input
-                    type="password"
-                    placeholder="create a Password"
-                    className="placeholder:text-sm border rounded-md py-2 px-2 mb-4 w-70"
-                />
+                <div className="relative">
+                    <input
+                        type={showPassword ? "text" : "password"}
+                        placeholder="create a Password"
+                        className="bg-white placeholder:text-sm w-full
+                            border border-gray-300 outline-none rounded-md py-2 px-2 pr-10 mb-4
+                            "
+                    />
+                    <button
+                        type="button"
+                        onClick={() => setShowPassword(!showPassword)}
+                        className="absolute right-3 top-1 translate-y-1.5">{showPassword ? <FiEye size={20}/> : <FiEyeOff size={20}/>}</button>
+                </div>
             </div>
-
-            <button>Sign Up</button>
         </>
     )
 }
