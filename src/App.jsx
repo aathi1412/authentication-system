d08ce2f12b96184b598b6be19c60560e410cf0c6
Aathi <aathithirunav@gmail.com>
2026-07-09 11:47:11 +0530

    feat(src): Add App component
    - Create App component with React Router
    - Define routes for Home, Register, and Login components

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
+
+import Register from "./components/Register.jsx";
+import Login from "./components/Login.jsx";
+import './App.css'
+import {Home} from "./components/Home.jsx";
+function App() {
+
+
+  return (
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/login" element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
