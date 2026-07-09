790e94bc7dc6113b92304f82bd434facc8afbb28
Aathi <aathithirunav@gmail.com>
2026-07-09 14:13:09 +0530

    feat(src): Add Home page styling
    - Added text-emerald-400 and text-2xl classes to the Welcome Home heading
    - Added a new paragraph with text-red-500 class

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,8 @@
 export function Home(){
     return(
         <div>
-            <h2>Welcome Home</h2>
+            <h2 className="text-emerald-400 text-2xl">Welcome Home</h2>
+            <p className="text-red-500">hi</p>
         </div>
     )
 }
