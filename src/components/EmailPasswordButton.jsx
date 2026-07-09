1f6b130460fe90973a44fe31ab3b2faf2e668a3f
Aathi <aathithirunav@gmail.com>
2026-07-09 11:47:16 +0530

    feat(src): Add EmailPasswordButton
    - Create a new component for handling email and password input

---
+++ src/components/EmailPasswordButton.jsx
@@ -0,0 +1,12 @@
+export function EmailPasswordButton(){
+    return (
+        <>
+            <input type="text" placeholder="Enter your Email"/>
+            <input type="password" placeholder="Enter your Password"/>
+
+            <button>Sign Up</button>
+        </>
+    )
+}
+
+export default EmailPasswordButton
