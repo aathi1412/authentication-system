2022e036aed43de6d88add5b067d4c89fe6340f0
Aathi <aathithirunav@gmail.com>
2026-07-09 11:47:39 +0530

    feat(src): Add Login component
    - Introduce a new Login component that renders an EmailPasswordButton

---
+++ src/components/Login.jsx
@@ -0,0 +1,11 @@
+import EmailPasswordButton from "./EmailPasswordButton.jsx";
+
+export function Login(){
+    return(
+        <>
+            <EmailPasswordButton/>
+        </>
+    )
+}
+
+export default Login;
