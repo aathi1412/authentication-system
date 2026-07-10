661f1c4ca5964b900cefd866003e0c74d2d7de5d
Aathi <aathithirunav@gmail.com>
2026-07-10 10:35:29 +0530

    feat(src): Add auth routes
    - Add routes for forgot password, reset password, and success message
    - Update import paths for Register and Login components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Register from "./components/Register.jsx";
-import Login from "./components/Login.jsx";
+import Register from "./components/auth/Register.jsx";
+import Login from "./components/auth/Login.jsx";
 import {Home} from "./components/Home.jsx";
 
 
 import './App.css'
 import TermsAndConditions from "./utils/TermsAndConditions.jsx";
+import ForgotPassword from "./components/auth/ForgotPassword.jsx";
+import {ResetPassword} from "./components/auth/ResetPassword.jsx";
+import SuccessMessage from "./components/auth/SuccessMessage.jsx";
 function App() {
 
 
@@ -16,6 +19,9 @@ function App() {
           <Route path="/register" element={<Register/>}/>
           <Route path="/login" element={<Login/>}/>
           <Route path="/TermsAndConditions" element={<TermsAndConditions/>} />
+          <Route path="/forgot-password" element={<ForgotPassword/>} />
+          <Route path="/reset-password" element={<ResetPassword/>} />
+          <Route path="/reset-password/success" element={<SuccessMessage/>}/>
       </Routes>
     </BrowserRouter>
   )
