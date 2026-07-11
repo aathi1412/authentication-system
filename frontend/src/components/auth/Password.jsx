b74cbc1b00a74094bd3db8e6186b28218aecb0e7
Aathi <aathithirunav@gmail.com>
2026-07-11 23:20:53 +0530

    feat(frontend): add password validation
    - added register prop to Password component
    - added error handling for password field

--- frontend/src/components/auth/Password.jsx
+++ frontend/src/components/auth/Password.jsx
@@ -1,26 +1,36 @@
 import {useState} from "react";
 import {FiEye, FiEyeOff} from "react-icons/fi";
 
-export function Password({ label, placeholder }){
+export function Password({ label, placeholder, register, errors }){
     const [showPassword, setShowPassword] = useState(false)
 
     return (
         <>
             <div>
                 <label className="block mb-1">{label}</label>
-                <div className="relative">
+                <div className="relative mb-3">
                     <input
                         type={showPassword ? "text" : "password"}
                         placeholder={placeholder}
+                        {...register("password")}
                         className="bg-white placeholder:text-sm placeholder:pl-3 w-full
-                            border border-gray-300 outline-none rounded-md py-2 px-2 pr-10 mb-4
+                            border border-gray-300 outline-none rounded-md py-2 px-2 pr-10 mb-1
                             transition-all duration-200 hover:shadow-md hover:bg-gray-100 hover:border-blue-600
                              focus:border-blue-600 focus:ring-2 focus:ring-blue-200 focus:shadow-lg "
                     />
                     <button
                         type="button"
                         onClick={() => setShowPassword(!showPassword)}
-                        className="absolute right-3 top-1 translate-y-1.5">{showPassword ? <FiEye size={20}/> : <FiEyeOff size={20}/>}</button>
+                        className="absolute right-3 top-1 translate-y-1.5"
+                    >
+                        {showPassword ? <FiEye size={20}/> : <FiEyeOff size={20}/>}
+                    </button>
+
+                    {errors.password &&
+                        <p className="mt-2 rounded-md bg-red-50 border border-red-200 px-3 py-2 text-sm text-red-700">
+                            {errors.password.message}
+                        </p>
+                    }
                 </div>
             </div>
         </>
