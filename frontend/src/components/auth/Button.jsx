a10f57b27077941ef8fe6219da77ce10553fc757
Aathi <aathithirunav@gmail.com>
2026-07-11 20:22:38 +0530

    fix(frontend): Fix Button component
    - Removed unnecessary comment for button type

--- frontend/src/components/auth/Button.jsx
+++ frontend/src/components/auth/Button.jsx
@@ -2,8 +2,10 @@ export function Button({text}){
     return(
         <div className="flex items-center justify-center my-2">
             <button
+                // type="button"
                 className="text-white font-bold border bg-[#2f6fed] w-full py-3 rounded-lg hover:bg-blue-700 "
-            >{text}
+            >
+                {text}
             </button>
         </div>
     )
