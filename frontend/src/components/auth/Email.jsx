4f9ba0937cb8b9f4a98583415f9806fdab7676e3
Aathi <aathithirunav@gmail.com>
2026-07-11 20:22:43 +0530

    feat(frontend): Add Email Validation
    - Update Email input type to 'email'
    - Add validation using register and errors props
    - Display error message below input field

--- frontend/src/components/auth/Email.jsx
+++ frontend/src/components/auth/Email.jsx
@@ -1,16 +1,24 @@
-export function Email(){
+
+
+export function Email({ register, errors}){
     return(
         <>
-            <div>
+            <div className="mb-3">
                 <label className="block mb-1">Email</label>
                 <input
-                    type="text"
+                    type="email"
                     placeholder="name@gmail.com"
+                    {...register("email")}
                     className="bg-white placeholder:text-sm placeholder:pl-3 w-full
-                            border border-gray-300 outline-none rounded-md py-2 px-2 mb-4
+                            border border-gray-300 outline-none rounded-md py-2 px-2 mb-1
                             transition-all duration-200 hover:shadow-md hover:bg-gray-100 hover:border-blue-600
                              focus:border-blue-600 focus:ring-2 focus:ring-blue-200 focus:shadow-lg"
                 />
+                {errors.email &&
+                    <p className="mt-2 rounded-md bg-red-50 border border-red-200 px-3 py-2 text-sm text-red-700">
+                        {errors.email.message}
+                    </p>
+                }
             </div>
         </>
     )
