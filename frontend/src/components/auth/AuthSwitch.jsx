b6689620aa7bd3c7b1aa59d6e085803c4d761bda
Aathi <aathithirunav@gmail.com>
2026-07-12 18:34:18 +0530

    refactor(frontend): Update AuthSwitch component
    - Changed AuthSwitch props from text to children
    - Updated Link text to use doAction prop

--- frontend/src/components/auth/AuthSwitch.jsx
+++ frontend/src/components/auth/AuthSwitch.jsx
@@ -1,10 +1,10 @@
 import {Link} from "react-router-dom";
 
-export function AuthSwitch({ text, path, doAction}){
+export function AuthSwitch({path, doAction, children}) {
     return(
         <div className="mt-4 text-gray-600 text-[13px] text-center">
             <p>
-                {text}
+                {children}
                 <Link to={path}>
                     <span className="text-[#3172ed] font-semibold hover:underline active:text-blue-300">
                         {doAction}
