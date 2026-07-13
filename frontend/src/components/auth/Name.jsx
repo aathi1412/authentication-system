d1ca14673994b81ddede9c9e325cf37ac243f7c5
Aathi <aathithirunav@gmail.com>
2026-07-13 23:07:53 +0530

    feat(frontend): Add username icon
    - Add CiUser icon from react-icons to the username input field
    - Update input field to have a relative container for icon positioning

--- frontend/src/components/auth/Name.jsx
+++ frontend/src/components/auth/Name.jsx
@@ -1,17 +1,22 @@
+import {CiUser} from "react-icons/ci";
+
 export function Name({register, errors}){
     return(
         <div className="mb-3">
             <label htmlFor="username" className="block mb-1">Full Name</label>
-            <input
-                id="username"
-                type="text"
-                placeholder="Roronoa Zoro"
-                {...register("name")}
-                className="bg-white placeholder:text-sm placeholder:pl-3 w-full
+            <div className="relative">
+                <CiUser className="absolute top-3.5 left-3"/>
+                <input
+                    id="username"
+                    type="text"
+                    placeholder="Roronoa Zoro"
+                    {...register("name")}
+                    className="bg-white placeholder:text-sm pl-10 w-full
                         border border-gray-300 outline-none rounded-md py-2 px-2 mb-1
                          transition-all duration-200 hover:shadow-md hover:bg-gray-100 hover:border-blue-600
                           focus:border-blue-600 focus:ring-2 focus:ring-blue-200 focus:shadow-lg"
-            />
+                />
+            </div>
             {errors.name &&
                 <p className="mt-2 rounded-md bg-red-50 border border-red-200 px-3 py-2 text-sm text-red-700">
                     {errors.name.message}
