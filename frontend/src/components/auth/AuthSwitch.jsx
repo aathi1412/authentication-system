e8c4868e6118bf5dd743452f721ada7fbca041a9
Aathi <aathithirunav@gmail.com>
2026-07-13 23:06:23 +0530

    feat(frontend): Add className prop to AuthSwitch
    - Added a new prop called className to the AuthSwitch component to allow for custom styling
    - Updated the Link component to use the className prop for styling

--- frontend/src/components/auth/AuthSwitch.jsx
+++ frontend/src/components/auth/AuthSwitch.jsx
@@ -1,14 +1,19 @@
 import {Link} from "react-router-dom";
 
-export function AuthSwitch({path, doAction, children}) {
+export function AuthSwitch({
+                               path,
+                               doAction,
+                               children,
+                               className = ""}) {
     return(
         <div className="mt-4 text-gray-600 text-[13px] text-center">
-            <p>
+            <p className={`text-sm ${className}`}>
                 {children}
-                <Link to={path}>
-                    <span className="text-[#3172ed] font-semibold hover:underline active:text-blue-300">
-                        {doAction}
-                    </span>
+                <Link
+                    to={path}
+                    className="text-[#3172ed] font-semibold hover:underline active:text-blue-300"
+                >
+                    {doAction}
                 </Link>
             </p>
         </div>
