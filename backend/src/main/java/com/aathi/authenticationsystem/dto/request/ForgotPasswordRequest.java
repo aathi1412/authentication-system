b1f3dc11a8023d75df9351d0501d27eb46b42536
Aathi <aathithirunav@gmail.com>
2026-07-13 23:07:20 +0530

    feat(backend): Add email validation
    - Added @Email annotation to validate email address in ForgotPasswordRequest

--- backend/src/main/java/com/aathi/authenticationsystem/dto/request/ForgotPasswordRequest.java
+++ backend/src/main/java/com/aathi/authenticationsystem/dto/request/ForgotPasswordRequest.java
@@ -1,5 +1,8 @@
 package com.aathi.authenticationsystem.dto.request;
 
+import jakarta.validation.constraints.Email;
+
 public record ForgotPasswordRequest(
+        @Email
         String email
 ) {}
