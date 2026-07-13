23c022d0643b762093b1840d05d559169c453b99
Aathi <aathithirunav@gmail.com>
2026-07-13 23:06:44 +0530

    feat(frontend): Add countdown hook
    - Implement useCountDown custom hook for countdown functionality

---
+++ frontend/src/customhooks/useCountDown.jsx
@@ -0,0 +1,24 @@
+import {useEffect, useState} from "react";
+
+export default function useCountDown(initialSeconds = 60){
+
+    const [seconds, setSeconds] = useState(initialSeconds);
+
+    useEffect(() => {
+        if (seconds <= 0) return;
+
+        const timer = setInterval(() => {
+            setSeconds((prev) => prev - 1);
+        }, 1000)
+
+        return () => {clearInterval(timer);}
+    }, [seconds]);
+
+    const start = () => { setSeconds(initialSeconds); };
+
+    return {
+        start,
+        seconds,
+        isRunning: seconds > 0
+    };
+}
