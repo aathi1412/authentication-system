19e89ec28afd6283fa318f809e4bf40e13633b3c
Aathi <aathithirunav@gmail.com>
2026-07-13 23:08:00 +0530

    feat(frontend): Add lock icon
    - Added LuLock icon to password input field

--- frontend/src/components/auth/Password.jsx
+++ frontend/src/components/auth/Password.jsx
@@ -1,5 +1,6 @@
 import {useState} from "react";
 import {FiEye, FiEyeOff} from "react-icons/fi";
+import {LuLock} from "react-icons/lu";
 
 export function Password({ label, placeholder, register, errors }){
     const [showPassword, setShowPassword] = useState(false)
@@ -9,11 +10,12 @@ export function Password({ label, placeholder, register, errors }){
             <div>
                 <label className="block mb-1">{label}</label>
                 <div className="relative mb-3">
+                    <LuLock className="absolute left-3 top-3.5 text-[#a1a1a1] pointer-events-none" />
                     <input
                         type={showPassword ? "text" : "password"}
                         placeholder={placeholder}
                         {...register("password")}
-                        className="bg-white placeholder:text-sm placeholder:pl-3 w-full
+                        className="bg-white placeholder:text-sm pl-10 w-full
                             border border-gray-300 outline-none rounded-md py-2 px-2 pr-10 mb-1
                             transition-all duration-200 hover:shadow-md hover:bg-gray-100 hover:border-blue-600
                              focus:border-blue-600 focus:ring-2 focus:ring-blue-200 focus:shadow-lg "
