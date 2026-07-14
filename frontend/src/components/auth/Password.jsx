1ff2e890687cf466075d906e28d21d46007828be
Aathi <aathithirunav@gmail.com>
2026-07-14 22:34:59 +0530

    refactor(frontend): Improve Password component
    - Add default name prop to Password component
    - Update register and error handling to use name prop

--- frontend/src/components/auth/Password.jsx
+++ frontend/src/components/auth/Password.jsx
@@ -2,7 +2,12 @@ import {useState} from "react";
 import {FiEye, FiEyeOff} from "react-icons/fi";
 import {LuLock} from "react-icons/lu";
 
-export function Password({ label, placeholder, register, errors }){
+export function Password({
+                             name = "password",
+                             label,
+                             placeholder,
+                             register,
+                             errors }){
     const [showPassword, setShowPassword] = useState(false)
 
     return (
@@ -14,7 +19,7 @@ export function Password({ label, placeholder, register, errors }){
                     <input
                         type={showPassword ? "text" : "password"}
                         placeholder={placeholder}
-                        {...register("password")}
+                        {...register(name)}
                         className="bg-white placeholder:text-sm pl-10 w-full
                             border border-gray-300 outline-none rounded-md py-2 px-2 pr-10 mb-1
                             transition-all duration-200 hover:shadow-md hover:bg-gray-100 hover:border-blue-600
@@ -28,9 +33,9 @@ export function Password({ label, placeholder, register, errors }){
                         {showPassword ? <FiEye size={20}/> : <FiEyeOff size={20}/>}
                     </button>
 
-                    {errors.password &&
+                    {errors[name] &&
                         <p className="mt-2 rounded-md bg-red-50 border border-red-200 px-3 py-2 text-sm text-red-700">
-                            {errors.password.message}
+                            {errors[name].message}
                         </p>
                     }
                 </div>
