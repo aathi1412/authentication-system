4bb25d0e0ef59436dd04b84db0c4da9e07271cba
Aathi <aathithirunav@gmail.com>
2026-07-28 23:54:43 +0530

    feat(frontend): Add Auth API
    - Implement register API endpoint
    - Implement login API endpoint

---
+++ frontend/src/api/authApi.js
@@ -0,0 +1,5 @@
+export const register = (data) => api.post("/register", data)
+export const login = (data) => api.post("/login", data)
+// export const login = (data) => api.post("/login", data)
+// export const login = (data) => api.post("/login", data)
+// export const login = (data) => api.post("/login", data)
