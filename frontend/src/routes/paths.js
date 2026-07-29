89992c2e8a4fbd24bfde222dac74c76e1226c16a
Aathi <aathithirunav@gmail.com>
2026-07-29 22:18:54 +0530

    refactor(frontend): organize route paths
    - group auth routes under a single object
    - define constants for auth and user paths

---
+++ frontend/src/routes/paths.js
@@ -0,0 +1,13 @@
+const Auth = "/auth";
+const USER = "/user";
+
+export const Paths = {
+    Auth: {
+        LOGIN: `/${Auth}/login`,
+        REGISTER: `/${Auth}/register`,
+        FORGOT_PASSWORD: `/${Auth}/forgot-password`,
+        REGISTER_PASSWORD: `/${Auth}/register-password`
+        EMAIL_SENT,
+        EMAIL_VERIFICATION: `/${Auth}/email-verification`,
+    }
+}
