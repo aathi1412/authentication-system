76b5996dfbfa362badc5fbd6f5e80efce72be86a
Aathi <aathithirunav@gmail.com>
2026-07-29 23:30:50 +0530

    refactor(frontend): Simplify App Routing
    - Removed redundant route imports
    - Replaced individual routes with AppRoutes component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,7 @@
 import {Toaster} from "react-hot-toast";
-import {BrowserRouter, Route, Routes} from "react-router-dom"
-import EmailSent from "./features/auth/EmailSent";
-import EmailVerification from "./features/auth/EmailVerification";
-import ForgotPassword from "./features/auth/pages/ForgotPassword";
-import Login from "./features/auth/pages/Login";
-import Register from "./features/auth/pages/Register";
-import ResetPassword from "./features/auth/pages/ResetPassword";
-import PasswordResetSuccess from "./features/auth/PasswordResetSuccess";
-import Home from "./features/Home";
+import {BrowserRouter} from "react-router-dom"
+import AppRoutes from "./routes/AppRoutes";
+
 import './App.css'
 
 function App() {
@@ -17,16 +11,7 @@ function App() {
         <>
             <Toaster/>
             <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Home/>}/>
-                    <Route path="/auth/register" element={<Register/>}/>
-                    <Route path="/auth/email-verification" element={<EmailVerification/>}/>
-                    <Route path="/auth/login" element={<Login/>}/>
-                    <Route path="/auth/forgot-password" element={<ForgotPassword/>}/>
-                    <Route path="/auth/forgot-password/email-sent" element={<EmailSent/>}/>
-                    <Route path="/auth/reset-password" element={<ResetPassword/>}/>
-                    <Route path="/auth/reset-password/success" element={<PasswordResetSuccess/>}/>
-                </Routes>
+                <AppRoutes/>
             </BrowserRouter>
         </>
 
