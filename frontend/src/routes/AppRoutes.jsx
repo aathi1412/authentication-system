0b2abdbbe65cb8491e519f9b836b648071437126
Aathi <aathithirunav@gmail.com>
2026-07-29 23:30:43 +0530

    feat(frontend): Add App Routes Configuration
    - Define AppRoutes component with React Router
    - Map routes to corresponding components

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,26 @@
+import {Route, Routes} from "react-router-dom"
+import EmailSent from "../features/auth/EmailSent";
+import EmailVerification from "../features/auth/EmailVerification";
+import ForgotPassword from "../features/auth/pages/ForgotPassword";
+import Login from "../features/auth/pages/Login";
+import Register from "../features/auth/pages/Register";
+import ResetPassword from "../features/auth/pages/ResetPassword";
+import PasswordResetSuccess from "../features/auth/PasswordResetSuccess";
+import Home from "../features/Home";
+import PATHS from "./paths"
+
+export default function AppRoutes(){
+    return (
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path={PATHS.USER.HOME} element={<Home/>}/>
+            <Route path={PATHS.AUTH.REGISTER} element={<Register/>}/>
+            <Route path={PATHS.AUTH.EMAIL_VERIFICATION} element={<EmailVerification/>}/>
+            <Route path={PATHS.AUTH.LOGIN} element={<Login/>}/>
+            <Route path={PATHS.AUTH.FORGOT_PASSWORD} element={<ForgotPassword/>}/>
+            <Route path={PATHS.AUTH.EMAIL_SENT} element={<EmailSent/>}/>
+            <Route path={PATHS.AUTH.RESET_PASSWORD} element={<ResetPassword/>}/>
+            <Route path={PATHS.AUTH.RESET_SUCCESS} element={<PasswordResetSuccess/>}/>
+        </Routes>
+    )
+}
