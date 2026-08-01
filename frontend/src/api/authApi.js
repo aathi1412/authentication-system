b4f4afeb79a7b0e91d94b962f3082db125e1209c
Aathi <aathithirunav@gmail.com>
2026-08-01 22:43:35 +0530

    feat(frontend): Add api import
    - Import api from axios to handle HTTP requests

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,3 +1,5 @@
+import api from "./axios";
+
 export const registerUser = (data) => api.post("/register", data)
 export const loginUser = (data) => api.post("/login", data)
 export const forgotPassword = (data) => api.post("/forgot-password", data)
