08046eb44335ae3395c18c82873fc8c4b4932f6d
Aathi <aathithirunav@gmail.com>
2026-08-04 20:43:22 +0530

    fix(frontend): Update reset password api
    - Changed resetPassword function to accept token and password as separate parameters

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -3,5 +3,4 @@ import api from "./axios";
 export const registerUser = (data) => api.post("/register", data)
 export const loginUser = (data) => api.post("/login", data)
 export const forgotPassword = (data) => api.post("/forgot-password", data)
-export const resetPassword = (token, data) => api.post("/reset-password", {token, password: data.password})
-// export const login = (data) => api.post("/login", data)
+export const resetPassword = (token, password) => api.post("/reset-password", {token, password})
