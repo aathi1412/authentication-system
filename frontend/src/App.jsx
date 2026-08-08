45720444538f3dc55ebd08833a3e97113150468a
Aathi <aathithirunav@gmail.com>
2026-08-08 23:16:07 +0530

    refactor(frontend): Simplify App Component
    - Removed unused BrowserRouter component
    - Replaced customToaster with CustomToaster
    - Removed AppRoutes import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,16 @@
-import {Toaster as customToaster} from "@/components/ui/toaster";
+import {Toaster as CustomToaster} from "@/components/ui/toaster";
 
 import {router} from "@/routes/router";
 import {Toaster} from "react-hot-toast";
-import {BrowserRouter, RouterProvider} from "react-router-dom"
-import AppRoutes from "./routes/AppRoutes";
+import {RouterProvider} from "react-router-dom"
 
 function App() {
-
 
     return (
         <>
             <Toaster/>
-            <BrowserRouter>
-                <AppRoutes/>
-            </BrowserRouter>
             <RouterProvider router={router} />
-            <customToaster />
+            <CustomToaster />
         </>
 
     )
