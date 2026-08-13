a88d5eb44e843433a4c5eeee4a6fd0f8615a4fde
Aathi <aathithirunav@gmail.com>
2026-08-13 23:11:05 +0530

    feat(backend): Add user retrieval and update functionality
    - Added method to retrieve user by email
    - Introduced method to update user information
    - Created method to update user profile image

--- backend/src/main/java/com/aathi/authenticationsystem/service/UserService.java
+++ backend/src/main/java/com/aathi/authenticationsystem/service/UserService.java
@@ -1,9 +1,15 @@
 package com.aathi.authenticationsystem.service;
 
+import com.aathi.authenticationsystem.dto.user.UpdateUserRequest;
+import com.aathi.authenticationsystem.dto.user.UserResponse;
+import com.aathi.authenticationsystem.exception.UserNotFoundException;
+import com.aathi.authenticationsystem.models.User;
 import com.aathi.authenticationsystem.repository.UserRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
@@ -14,6 +20,22 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    public User getUserByEmail(String email){
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UserNotFoundException("User not found"));
+    }
+
+    public UserResponse getUserResponse(UpdateUserRequest request){
+        User user = getUserByEmail(request.);
+    }
+
+    public UserResponse updateUser(@Valid UpdateUserRequest request) {
+
+        User savedUser = userRepository.
+    }
+
+    public void updateProfileImage(MultipartFile image) {
+    }
     @Transactional
     public void lockOrUnlockAccount(String email){
 
@@ -55,4 +77,7 @@ public class UserService {
         userRepository.findByEmail(email)
                 .ifPresent(user -> user.setFailedAttempts(0));
     }
+
+
+
 }
