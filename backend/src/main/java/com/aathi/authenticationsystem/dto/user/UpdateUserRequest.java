f80b0051303303dd204a7c32f4bea679947fd38a
Aathi <aathithirunav@gmail.com>
2026-08-13 23:11:33 +0530

    feat(backend): Add UpdateUserRequest DTO
    - Create a new data transfer object for user updates

---
+++ backend/src/main/java/com/aathi/authenticationsystem/dto/user/UpdateUserRequest.java
@@ -0,0 +1,12 @@
+package com.aathi.authenticationsystem.dto.user;
+
+import lombok.Getter;
+
+@Getter
+public class UpdateUserRequest {
+
+    private String name;
+    private String phone;
+    private String bio;
+    private String profileImage;
+}
