cf48e8ca866d61d2f343a214c133db4963cfc32a
Aathi <aathithirunav@gmail.com>
2026-08-14 22:46:03 +0530

    refactor(backend): Enhance user controller
    - Replace UpdateUserRequest with AuthenticationPrincipal in getUser endpoint
    - Update getUser endpoint to return user details
    - Modify updateUser endpoint to use CustomUserDetails and UpdateUserRequest

--- backend/src/main/java/com/aathi/authenticationsystem/controller/UserController.java
+++ backend/src/main/java/com/aathi/authenticationsystem/controller/UserController.java
@@ -3,10 +3,12 @@ package com.aathi.authenticationsystem.controller;
 
 import com.aathi.authenticationsystem.dto.user.UpdateUserRequest;
 import com.aathi.authenticationsystem.dto.user.UserResponse;
+import com.aathi.authenticationsystem.security.userdetails.CustomUserDetails;
 import com.aathi.authenticationsystem.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,14 +20,14 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/me")
-    public ResponseEntity<UserResponse> getUser(@Valid UpdateUserRequest request){
-        userService.updateUser(request);
-        return ResponseEntity.ok(new UserResponse());
+    public ResponseEntity<UserResponse> getUser(@AuthenticationPrincipal CustomUserDetails userDetails){
+        return ResponseEntity.ok(userService.getUserResponse(userDetails));
     }
 
     @PutMapping("/me")
-    public String updateUser(){
-        return "Welcome Login Successfull!";
+    public ResponseEntity<UserResponse> updateUser(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                                   @Valid @RequestBody UpdateUserRequest request){
+        return ResponseEntity.ok(userService.updateUser(userDetails, request));
     }
 
     @PostMapping("/profile/image")
