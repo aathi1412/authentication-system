f8c02ab2149301712401126087079ea47ba529fe
Aathi <aathithirunav@gmail.com>
2026-08-23 22:30:05 +0530

    feat(auth): Redirect root path to login page
    - Previously, the root path redirected to a user's home page.
    - This ensures unauthenticated users are consistently directed to the authentication flow upon initial access.

--- frontend/src/routes/router.jsx
+++ frontend/src/routes/router.jsx
@@ -67,7 +67,7 @@ function withSuspense(element) {
 export const router = createBrowserRouter([
         {
             path: "/",
-            element: <Navigate to={PATHS.USER.HOME} replace />,
+            element: <Navigate to={PATHS.AUTH.LOGIN} replace />,
         },
 
         // Auth
