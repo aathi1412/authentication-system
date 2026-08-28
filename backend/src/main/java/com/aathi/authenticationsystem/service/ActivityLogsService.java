747a198835d07886e275eb06a6e1275460748419
Aathi <aathithirunav@gmail.com>
2026-08-28 23:26:03 +0530

    refactor(activity-logs): Streamline activity log saving for authentication
    - Remove `type` parameter from `saveActivityLog` as it is no longer used.
    - Explicitly set activity `category` to `AUTHENTICATION` within the method.

--- backend/src/main/java/com/aathi/authenticationsystem/service/ActivityLogsService.java
+++ backend/src/main/java/com/aathi/authenticationsystem/service/ActivityLogsService.java
@@ -1,5 +1,6 @@
 package com.aathi.authenticationsystem.service;
 
+import com.aathi.authenticationsystem.enums.ActivityCategory;
 import com.aathi.authenticationsystem.models.ActivityLogs;
 import com.aathi.authenticationsystem.models.User;
 import com.aathi.authenticationsystem.repository.ActivityLogsRepository;
@@ -15,15 +16,14 @@ public class ActivityLogsService {
     private final ActivityLogsRepository activityLogsRepository;
     private final UserService userService;
 
-    public void saveActivityLog(Long userId, String title, String description, String type, String category){
+    public void saveActivityLog(Long userId, String title, String description, String category){
 
         User user = userService.getUserById(userId);
         ActivityLogs activityLogs = ActivityLogs.builder()
                 .user(user)
-                .type(type)
                 .title(title)
                 .description(description)
-                .category(category)
+                .category(ActivityCategory.AUTHENTICATION)
                 .build();
 
         activityLogsRepository.save(activityLogs);
