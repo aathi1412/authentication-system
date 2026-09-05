0029e1d7123276d4ce1a3b54a903183a7c319337
Aathi <aathithirunav@gmail.com>
2026-09-05 23:25:08 +0530

    style(app): Clean up whitespace in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,12 @@ import {Toaster} from "react-hot-toast";
 import {RouterProvider} from "react-router-dom"
 
 function App() {
-
     return (
         <>
             <Toaster/>
             <RouterProvider router={router} />
             <CustomToaster />
         </>
-
     )
 }
 
