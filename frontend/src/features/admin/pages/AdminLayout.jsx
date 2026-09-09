ca128acaabcf511ed82b1180de00e008f0ee4e08
Aathi <aathithirunav@gmail.com>
2026-09-09 23:04:28 +0530

    feat(admin): Create AdminLayout component
    This component will serve as the base layout for all admin-specific routes, providing a common structure and rendering nested routes via the `Outlet` component.

---
+++ frontend/src/features/admin/pages/AdminLayout.jsx
@@ -0,0 +1,13 @@
+import {Outlet} from "react-router-dom";
+
+function AdminLayout() {
+    return (
+        <div>
+            <h1>Admin Layout</h1>
+
+            <Outlet />
+        </div>
+    );
+}
+
+export default AdminLayout;
