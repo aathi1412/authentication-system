2439fcf16ff7bfb849573a3a11179f8150ab8eab
Aathi <aathithirunav@gmail.com>
2026-09-11 23:43:43 +0530

    feat(admin): Add UsersPage to display admin users
    - Add useState and useEffect to fetch admin users from API
    - Show loading indicator while fetching
    - Render list of user details (name, email, role)

--- frontend/src/features/admin/pages/UsersPage.jsx
+++ frontend/src/features/admin/pages/UsersPage.jsx
@@ -1,9 +1,40 @@
+import {useEffect, useState} from 'react';
+import {apiClient} from '../../../lib/axiosClient'
+
 function UsersPage() {
+    const [loading, setLoading] = useState(true);
+    const [users, setUsers] = useState();
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try{
+                const response = await apiClient.get("/admin/users");
+                console.log(response.data);
+                setUsers(response.data.content)
+            }
+            catch (err) {
+                console.log(err);
+            }
+            finally{
+                setLoading(false);
+            }
+        }
+
+        fetchUsers();
+    }, []);
+
     return (
-        <div>
+        <>
             <h1>Users</h1>
-            <p>Manage users here.</p>
-        </div>
+            {loading && <p>Loading User data....</p>}
+            {users && users.map((user) => (
+                <div key={user.id}>
+                    <p>{user.name}</p>
+                    <p>{user.email}</p>
+                    <p>{user.role}</p>
+                </div>
+            ))}
+        </>
     );
 }
 
