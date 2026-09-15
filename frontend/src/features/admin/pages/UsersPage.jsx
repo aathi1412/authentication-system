08cbea52ae9f2a57ea5e1cfa017b0ddd3d461bb4
Aathi <aathithirunav@gmail.com>
2026-09-15 23:03:36 +0530

    feat(admin): Add email column to UsersPage table
    - Add email column to the UsersPage table to display user email addresses

--- frontend/src/features/admin/pages/UsersPage.jsx
+++ frontend/src/features/admin/pages/UsersPage.jsx
@@ -44,6 +44,7 @@ function UsersPage() {
                     <tr key={user.id}>
                         <td>{user.id}</td>
                         <td>{user.name}</td>
+
                         <td>{user.email}</td>
                         <td>{user.role}</td>
                         <td>
