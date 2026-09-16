82313752b7b321611ea9fef53ff78ee0dec76627
Aathi <aathithirunav@gmail.com>
2026-09-16 22:03:53 +0530

    refactor(paths): Add ADMIN route constants
    - Define ADMIN HOME path constant

--- frontend/src/routes/paths.js
+++ frontend/src/routes/paths.js
@@ -17,6 +17,10 @@ const PATHS = {
         PROFILE: `${ACCOUNT}/profile`,
         SECURITY: `${ACCOUNT}/security`,
         ACTIVITY: `${ACCOUNT}/activity`,
+    },
+
+    ADMIN: {
+        HOME: '/admin'
     }
 }
 
